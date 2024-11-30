b54804762a6cca6f5682ac5a670def9476ba7580
Sekky61 <misa@majer.cz>
2024-11-30 17:38:02 +0100

    lodash example call

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log("Starting...");
+// Example of using a library from npm
+import _ from 'lodash';
+
+function main() {
+  const chunks = _.chunk(['a', 'b', 'c', 'd'], 2);
+  console.log("Starting...", chunks);
+}
+
+// Call main here to not lose it after bundling
+main();
 
