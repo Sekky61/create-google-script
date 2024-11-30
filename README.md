444bac4cdbef983ffaf69ea17f70c478a694796f
Sekky61 <misa@majer.cz>
2024-11-30 17:15:07 +0100

    clasp ignore, deploy script

--- README.md
+++ README.md
@@ -23,7 +23,8 @@ For interacting with clasp, read their [docs](https://github.com/google/clasp/bl
 
 `bun run login` will log you in globally using your browser.
 After logging in, either `bun run create` or `bun run clone` to pair with a project.
-This creates a `.clasp.json` file with the info.
+This creates a `.clasp.json` file with the info. You may not want to commit it, because it is user-specific.
 
 Now with the project paired, `bun run push` or `bun run watch` will update the project on google cloud.
 
+`bun run deploy -- "description"`
---
+++ deploy.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env bun
+
+import { $ } from "bun";
+
+const claspCmd = "./node_modules/.bin/clasp"; // local clasp path
+
+async function createVersion(description = "auto-deployed version") {
+  const versionOutput = await $`${claspCmd} version "${description}"`;
+  const buffer = versionOutput.stdout.toString();
+  // Output looks like: `Created version 7.`
+  const version = buffer.match(/Created version (\d+)\./)[1];
+  return version;
+}
+
+async function deployVersion(version, description = "auto deployment") {
+  await $`${claspCmd} deploy -V ${version} -d "${description}"`;
+}
+
+async function pushCode() {
+  await $`${claspCmd} push`;
+}
+
+function log(message: string) {
+  console.log(`[deploy] ${message}`);
+}
+
+(async () => {
+  const description = process.argv[2] || "auto-deployed";
+  log(`pushing changes...`);
+  await pushCode();
+  log(`pushed changes`);
+  const version = await createVersion(description);
+  log(`created version "${version}"`);
+  log(`deploying version "${version}"...`);
+  await deployVersion(version, description);
+  log(`deployed version "${version}"`);
+})();
