bc5bd4d533dc915e6a31f388da337ca8e2a9cb88
Sekky61 <misa@majer.cz>
2024-11-30 18:45:44 +0100

    Adds example test mock of a sheet

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 // Example of using a library from npm
 import _ from 'lodash';
 
+export function readRow(sheet: GoogleAppsScript.Spreadsheet.Sheet, rowNumber: number): string[] {
+  const row = sheet.getRange(rowNumber, 1, 1, sheet.getLastColumn()).getValues()[0];
+  return row.map((cell) => cell.toString().trim());
+}
+
 function main() {
   const chunks = _.chunk(['a', 'b', 'c', 'd'], 2);
   console.log("Starting...", chunks);
 }
 
 // Call main here to not lose it after bundling
-main();
+if (import.meta.main) {
+  main();
+}
 
---
+++ tests/example.test.ts
@@ -0,0 +1,20 @@
+import { expect, test, mock } from "bun:test";
+import { readRow } from "../src/index";
+
+// Mock the necessary functions
+const sheet = {
+  getRange: mock(() => {
+    return {
+      getValues: () => [[1, 2, 3, 4]],
+    };
+  }),
+  getLastColumn: mock(() => 4),
+} as any as GoogleAppsScript.Spreadsheet.Sheet;
+
+test("2 + 2", () => {
+  expect(2 + 2).toBe(4);
+});
+
+test("readRow", () => {
+  expect(readRow(sheet, 1)).toEqual(["1", "2", "3", "4"]);
+});
