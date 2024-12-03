54645ca9ff41b89d2dd9f33be75060acc87b84af
Sekky61 <misa@majer.cz>
2024-12-03 12:18:23 +0100

    range links utils

--- src/utils.ts
+++ src/utils.ts
@@ -38,3 +38,43 @@ export function mapArrayToObjects(
         ),
     );
 }
+
+/**
+ * Create an internal or external link similar to "#gid=0&range=B2".
+ * If spreadsheetId is not provided, the link will be local.
+ */
+export function rangeLinkRaw(
+    sheetId: string,
+    a1Range: string,
+    spreadsheetId?: string,
+) {
+    const url = spreadsheetId
+        ? `https://docs.google.com/spreadsheets/d/${spreadsheetId}`
+        : "";
+    return `${url}#gid=${sheetId}&range=${a1Range}`;
+}
+
+/**
+ * Create an local (default) or external link to a range.
+ * Clicking on local link is more pleasant for the user.
+ */
+export function rangeLink(range: Range, local = true) {
+    const sheet = range.getSheet();
+    const spreadsheetId = local ? undefined : sheet.getParent().getId();
+    return rangeLinkRaw(
+        sheet.getSheetId().toString(),
+        range.getA1Notation(),
+        spreadsheetId,
+    );
+}
+
+/**
+ * Create a link in a range
+ */
+export function setLink(range: Range, url: string, label?: string) {
+    const richValue = SpreadsheetApp.newRichTextValue()
+        .setText(label ?? url)
+        .setLinkUrl(url)
+        .build();
+    range.setRichTextValue(richValue);
+}
